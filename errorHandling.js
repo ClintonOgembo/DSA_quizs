3d21f091c32c2b62a42cfd41d975d61dedc5595c
ClintonOgembo <clintonogembo70@gmail.com>
2025-10-25 12:49:53 +0300

    error handling

--- errorHandling.js
+++ errorHandling.js
@@ -3,5 +3,21 @@ try {
     let x = y + 1;
     console.log("This will not run");
 } catch (error) {
-    console.log("Error Message: ", error.message);
+    console.log("An Error occured: ", error.message);
+} finally {
+    console.log("This always runs!");
+}
+
+// Using Throw
+function divide(a, b) {
+    if (b == 0) {
+        throw new Error("Cannot be divided by zero");
+    }
+    return a / b;
+}
+
+try {
+    console.log(divide(10, 0));
+} catch (error) {
+    console.log("An error occured: ", error.message);
 }
