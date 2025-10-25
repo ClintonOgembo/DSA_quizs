6e2f6aa4a4aac229283bcfbc938bdb93fa97e745
ClintonOgembo <clintonogembo70@gmail.com>
2025-10-25 11:06:20 +0300

    js brush up

--- brushUp.js
+++ brushUp.js
@@ -21,3 +21,36 @@ console.log(Addition(10, 7))
 // Arrow function
 const Multiplication = (n, m) => n * m;
 console.log(Multiplication(3, 4));
+
+// parameters and args
+function greetings(name) {
+    console.log(`Hello ${name}`);
+}
+greetings("Clinton");
+
+//default args
+function Greet(name = "Guest") {
+    console.log(`Hi ${name}`);
+}
+Greet();
+
+// use of return
+function getArea(radius) {
+    return Math.PI * radius ** 2;
+}
+
+console.log(getArea(5));
+
+//immediately invoked function expression
+(function () {
+    console.log("This runs immediately!");
+})();
+
+// This keyword
+const player = {
+    name: "Njeri",
+    showThis() {
+        console.log(this.name);
+    }
+};
+player.showThis();
