368844beea20a89588fa92fdbcdc97567d59fa30
ClintonOgembo <clintonogembo70@gmail.com>
2025-10-27 00:27:40 +0300

    done

--- ContainDuplicates.js
+++ ContainDuplicates.js
@@ -1,6 +1,13 @@
 /*Given an integer array nums, return true if any value appears more than once in the array,
  otherwise return false.
  */
+
+/** My Solution
+ * Time Complexity
+The loop runs once through nums → O(n)
+ * Space Complexity
+In the worst case (no duplicates), all n elements are stored in the Set => Space Complexity: O(n)
+*/
 class Solution {
     /**
      * @param {number[]} nums
@@ -23,9 +30,25 @@ const solu = new Solution();
 console.log(solu.hasDuplicate([1, 2, 3, 4, 4]));
 console.log(solu.hasDuplicate([1, 2, 3, 4, 5]));
 
-/*Time Complexity
-The loop runs once through nums → O(n)
 
-Space Complexity
-In the worst case (no duplicates), all n elements are stored in the Set => Space Complexity: O(n)
-*/
+
+/** Second Solution in neetcode
+ * SPACE COMPLEXITY O(1) or O(log n)
+ * TIME COMPLEXITY O(n)
+ */
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        nums.sort((a, b) => a - b);  // Sort the array
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] === nums[i - 1]) {       // Compare each element to the previous one
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
