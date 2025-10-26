20d0523b6db260ce5b1bf301539dce7614ea1afd
ClintonOgembo <clintonogembo70@gmail.com>
2025-10-27 00:15:09 +0300

    done

--- ContainDuplicates.js
+++ ContainDuplicates.js
@@ -1,3 +1,31 @@
 /*Given an integer array nums, return true if any value appears more than once in the array,
  otherwise return false.
  */
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const newSet = new Set();
+        for (let i of nums) {
+            if (newSet.has(i)) {
+                return true;
+            }
+            newSet.add(i);
+        }
+        return false;
+    }
+
+}
+
+const solu = new Solution();
+console.log(solu.hasDuplicate([1, 2, 3, 4, 4]));
+console.log(solu.hasDuplicate([1, 2, 3, 4, 5]));
+
+/*Time Complexity
+The loop runs once through nums → O(n)
+
+Space Complexity
+In the worst case (no duplicates), all n elements are stored in the Set => Space Complexity: O(n)
+*/
