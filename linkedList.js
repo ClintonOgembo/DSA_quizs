9f3c9cc3b512398b694d031c0787752fce6b4c79
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-04 17:51:05 +0300

    push

--- linkedList.js
+++ linkedList.js
@@ -69,6 +69,19 @@ class LinkedList {
         this.length++;
         return this;
     }
+    /// WRITE PUSH METHOD HERE ///
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
 
 
     printList() {
