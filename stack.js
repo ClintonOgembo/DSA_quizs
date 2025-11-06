612806ef240497a9cac1547b240d8b35134c4a2f
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-06 16:04:20 +0300

    class node,stack and push method

---
+++ stack.js
@@ -0,0 +1,72 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.top = newNode;
+        this.length = 1;
+    }
+
+    printStack() {
+        let temp = this.top;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getTop() {
+        if (this.top === null) {
+            console.log("Top: null");
+        } else {
+            console.log("Top: " + this.top.value);
+        }
+    }
+
+    getLength() {
+        console.log("Length: " + this.length);
+    }
+
+    makeEmpty() {
+        this.top = null;
+        this.length = 0;
+    }
+
+    /// WRITE PUSH METHOD HERE ///
+    push(value) {
+        const newNode = new Node(value);
+        newNode.next = this.top;
+        this.top = newNode;
+        this.length++;
+        return this;
+    }
+
+}
+
+
+
+let myStack = new Stack(2);
+
+console.log("Before push():");
+console.log("--------------");
+myStack.getTop();
+myStack.getLength();
+
+console.log("\nStack:");
+myStack.printStack();
+
+myStack.push(1);
+
+console.log("\n\nAfter push():");
+console.log("-------------");
+myStack.getTop();
+myStack.getLength();
+
+console.log("\nStack:");
+myStack.printStack();
+
