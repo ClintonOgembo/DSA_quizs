c159c1a37bb6a09e0627f5db9307a024d5442b05
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-08 22:39:09 +0300

    insert()

--- BST.js
+++ BST.js
@@ -12,6 +12,35 @@ class BST {
     constructor() {
         this.root = null;
     }
+    /// WRITE INSERT METHOD HERE ///
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+
+        let temp = this.root;
+        while (true) {
+            if (value === temp.value) return undefined;
+
+            if (value < temp.value) {
+                if (!temp.left) {
+                    temp.left = newNode;
+                    return this;
+                }
+                else temp = temp.left;
+            }
+            else if (value > temp.value) {
+                if (!temp.right) {
+                    temp.right = newNode;
+                    return this;
+                }
+                else temp = temp.right;
+            }
+        }
+    }
+
 }
 
 
