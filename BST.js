dcc8255f12d33ac064ca5bd4af44721c461f0ca0
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-09 23:18:22 +0300

    comments

--- BST.js
+++ BST.js
@@ -72,3 +72,78 @@ if (myBST.root === null) {
 } else {
     console.log("Root:", myBST.root.value);
 }
+
+// // WRITE NODE CLASS HERE //
+// class Node {
+//     constructor(value) {
+//         this.value = value;
+//         this.left = null;
+//         this.right = null;
+//     }
+
+// }
+
+// class BST {
+//     constructor() {
+//         this.root = null;
+//     }
+//     /// WRITE INSERT METHOD HERE ///
+//     insert(value) {
+//         const newNode = new Node(value);
+//         if (!this.root) {
+//             this.root = newNode;
+//             return this;
+//         }
+
+//         let temp = this.root;
+//         while (true) {
+//             if (value === temp.value) return undefined;
+
+//             if (value < temp.value) {
+//                 if (!temp.left) {
+//                     temp.left = newNode;
+//                     return this;
+//                 }
+//                 else temp = temp.left;
+//             }
+//             else if (value > temp.value) {
+//                 if (!temp.right) {
+//                     temp.right = newNode;
+//                     return this;
+//                 }
+//                 else temp = temp.right;
+//             }
+//         }
+//     }
+//     /// WRITE CONTAINS METHOD HERE ///
+//     contains(value) {
+//         if (!this.root) return false;
+//         let temp = this.root;
+//         if (value === temp.value) return true;
+
+//         while (temp) {
+//             if (value < temp.value) {
+//                 if (temp.value === value) return true;
+//                 else temp = temp.left;
+//             }
+//             else {
+//                 if (temp.value === value) return true;
+//                 else temp = temp.right;
+//             }
+//         }
+//         return false;
+
+//     }
+
+
+// }
+
+
+
+// let myBST = new BST();
+
+// if (myBST.root === null) {
+//     console.log("Root: null");
+// } else {
+//     console.log("Root:", myBST.root.value);
+// }
