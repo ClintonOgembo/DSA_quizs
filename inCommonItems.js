57bd845c791bcff55ccfd0f5b5fba6604a3fecae
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-12 13:34:40 +0300

    using hashmap

--- inCommonItems.js
+++ inCommonItems.js
@@ -1,3 +1,4 @@
+// Using Object
 function itemInCommon(arr1, arr2) {
     let obj = {};
     for (let i = 0; i < arr1.length; i++) {
@@ -5,6 +6,18 @@ function itemInCommon(arr1, arr2) {
     }
     for (let j = 0; j < arr2.length; j++) {
         if (obj[arr2[j]]) return true;
+    }
+    return false;
+}
+
+// Using Hash Map
+function itemsInCommon(array1, array2) {
+    let myMap = new Map();
+    for (let i of array1) {
+        myMap.set(i, true);
+    }
+    for (let j of array2) {
+        if (myMap.has(j)) return true;
     }
     return false;
 }
