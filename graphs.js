4cef9aca1df23466a8d642dd2188217a62bfdfce
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-13 23:33:10 +0300

    remove vertex

--- graphs.js
+++ graphs.js
@@ -58,8 +58,50 @@ class Graph {
 
 
 
-let myGraph = new Graph();
-myGraph.addVertex("A");
-myGraph.addVertex("B");
-myGraph.printGraph();
+// Test 1
+console.log("// Test 1: Remove an existing vertex");
+const g1 = new Graph();
+g1.addVertex('A');
+g1.addVertex('B');
+g1.addEdge('A', 'B');
+g1.removeVertex('A');
+g1.printGraph();
+
+// Test 2
+console.log("\n// Test 2: Remove a vertex that does not exist");
+const g2 = new Graph();
+console.log(g2.removeVertex('C'));  // Expected: undefined
+g2.printGraph();
+
+// Test 3
+console.log("\n// Test 3: Remove vertex that is self-connected");
+const g3 = new Graph();
+g3.addVertex('A');
+g3.addEdge('A', 'A');
+g3.removeVertex('A');
+g3.printGraph();
+
+// Test 4
+console.log("\n// Test 4: Remove multiple vertices");
+const g4 = new Graph();
+g4.addVertex('A');
+g4.addVertex('B');
+g4.addVertex('C');
+g4.addEdge('A', 'B');
+g4.addEdge('B', 'C');
+g4.removeVertex('A');
+g4.removeVertex('C');
+g4.printGraph();
+
+// Test 5
+console.log("\n// Test 5: Remove vertex that has multiple connections");
+const g5 = new Graph();
+g5.addVertex('A');
+g5.addVertex('B');
+g5.addVertex('C');
+g5.addEdge('A', 'B');
+g5.addEdge('A', 'C');
+g5.removeVertex('A');
+g5.printGraph();
+
 
