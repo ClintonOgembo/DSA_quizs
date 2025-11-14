a6b5533f170301914767d284537ecb359fad0ffa
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-14 22:50:40 +0300

    instances

--- heaps.js
+++ heaps.js
@@ -27,3 +27,19 @@ class Heap {
         }
     }
 }
+
+const myHeap = new Heap();
+myHeap.insert(99);
+myHeap.insert(72);
+myHeap.insert(61);
+myHeap.insert(58);
+
+console.log(myHeap.getHeap());
+
+myHeap.insert(100);
+
+console.log(myHeap.getHeap());
+
+myHeap.insert(75);
+
+console.log(myHeap.getHeap());
