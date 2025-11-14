9987075b30e51aac360d8bdab5996fa785cbcaa4
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-14 22:43:48 +0300

    insert

--- heaps.js
+++ heaps.js
@@ -11,9 +11,14 @@ class Heap {
         return 2 * parentIndex + 2;
     }
     #parentIndex(index) {
-        return Math.floor((index - 1) / 2);
+        return Math.floor((index - 1) / 2); // floor removes anything after decimal point
     }
     #swap(index1, index2) {
         [this.#heap[index1], this.#heap[index2]] = [this.#heap[index2], this.#heap[index1]]
+    }
+
+    insert(value) {
+        this.#heap.push(value);
+        let current = this.#heap.length - 1;
     }
 }
