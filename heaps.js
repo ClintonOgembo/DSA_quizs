cdfad554d5b608745ec3a000d8ef0220b93fc059
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-14 22:32:59 +0300

    heaps

---
+++ heaps.js
@@ -0,0 +1,16 @@
+class Heap {
+    #heap = [];
+    getHeap() {
+        return [...this.#heap]
+    }
+
+    #leftChild(index) {
+        return 2 * parentIndex + 1;
+    }
+    #rightChild(index) {
+        return 2 * parentIndex + 2;
+    }
+    #parentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+}
