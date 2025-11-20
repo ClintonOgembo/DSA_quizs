43783a04bd08bee2169541870b2e00c3dd27e89a
ClintonOgembo <clintonogembo70@gmail.com>
2025-11-21 01:31:16 +0300

    encode()

---
+++ EncodeDecode.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const newStr = strs.join('')
+        console.log(newStr);
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) { }
+}
