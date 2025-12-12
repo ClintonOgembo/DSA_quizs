e02c44d94ef733cf0db21541506ed166e60471f5
ClintonOgembo <clintonogembo70@gmail.com>
2025-12-12 14:00:30 +0300

    leap year challenge

--- leapyear.js
+++ leapyear.js
@@ -46,6 +46,4 @@ function isLeap(year) {
     if ((year % 4) === 0 && (year % 400) === 0) return "Leap year.";
     else if (((year % 4) === 0) || ((year % 100) === 0) && ((year % 400) === 0)) return "Leap year.";
     else return "Not leap year.";
-
-
 }
